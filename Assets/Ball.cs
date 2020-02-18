eb62f0af6c47a1e3cf19acd6c6b4bad483cc8ef7
AndreyDlxxvii <Dlxxvii@gmail.com>
2020-02-18 17:05:54 +0300

    Отражение

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private bool flag = false;
-    private Vector2 vector2 = Vector2.up;
+    private Vector2 vector2 = Vector2.left;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
+        var temp = GetComponent<Rigidbody2D>().position;
+        print(temp);
 
         flag = true;
         
@@ -21,6 +22,7 @@ public class Ball : MonoBehaviour
         if (flag == true)
         {
             vector2 = Vector2.down;
+            vector2.reflect
         }
     }
 }
