24466a83ec248c86a1a8ecff519f7eed0f5e709a
AndreyDlxxvii <Dlxxvii@gmail.com>
2020-02-18 16:12:18 +0300

    Начало проекта

---
+++ Assets/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private bool flag = false;
+    private Vector2 vector2 = Vector2.up;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        flag = true;
+        
+    }
+
+
+    void Update()
+    {
+        transform.Translate(vector2 * Time.deltaTime);
+        if (flag == true)
+        {
+            vector2 = Vector2.down;
+        }
+    }
+}
