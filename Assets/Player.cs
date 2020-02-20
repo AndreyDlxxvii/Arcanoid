c839cb4403f24a2362ccd2bbd1ec9bfd68e51471
AndreyDlxxvii <Dlxxvii@gmail.com>
2020-02-20 16:44:44 +0300

    управление

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,28 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public GameObject player;
 
-    public Rigidbody2D _rb;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-       _rb.velocity = new Vector2(0f, 0f);
-    }
 
-    private void Start()
-    {
-       _rb = GetComponent<Rigidbody2D>();
-    }
-    private void FixedUpdate()
-    {
-        Move();
-    }
 
-    private void Move()
+
+        void Update()
     {
-        float movePlayer = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(movePlayer, 0f);
-        transform.Translate(movement * speed * Time.fixedDeltaTime);
-        //_rb.AddForce(movement * speed);
+        float i = Input.GetAxis("Horizontal");
+        print(i);
+  
+        float xPos = speed * i * Time.deltaTime;
+
+        player.transform.position = new Vector2(xPos, -4.53f);
+
+
     }
 }
