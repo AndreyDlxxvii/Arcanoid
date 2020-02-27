02d1d7658d5b93b19d1699239a1676ef7ffd2b31
AndreyDlxxvii <Dlxxvii@gmail.com>
2020-02-27 10:39:30 +0300

    temp

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public float power;
+    
     private void Start()
     {
         var vect = new Vector2(1.1f, 1.2f);
         var force = GetComponent<Rigidbody2D>();
-        force.AddForce(vect * Time.deltaTime * power);
+        force.AddForce(vect * Time.deltaTime * 10000);
     }
 
 
